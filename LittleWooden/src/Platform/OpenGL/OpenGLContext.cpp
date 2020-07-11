64b448760f67cc8bde9c25aeeedf33f0dc8aee9d
Dylan Littlewood <dylanlittlewood@live.co.uk>
2020-07-11 10:57:51 +0100

    Drawing my first triangle!

--- LittleWooden/src/Platform/OpenGL/OpenGLContext.cpp
+++ LittleWooden/src/Platform/OpenGL/OpenGLContext.cpp
@@ -15,6 +15,11 @@ namespace LittleWooden {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		LW_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		LW_CORE_INFO("OpenGL Info:----------------------------");
+		LW_CORE_INFO("   Vendor: {0}", glGetString(GL_VENDOR));
+		LW_CORE_INFO(" Renderer: {0}", glGetString(GL_RENDERER));
+		LW_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 	void OpenGLContext::SwapBuffers()
 	{
